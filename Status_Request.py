cc4cbe8775c4687f766eaddec6cba149de420be8
samuel-sentry <60991910+samuel-sentry@users.noreply.github.com>
2020-02-12 16:59:48 -0800

    Update Status_Request.py

--- Status_Request.py
+++ Status_Request.py
@@ -1,18 +1,35 @@
 import requests
 
 sentry_id = "R6GNAS-B"
+camera = "Cam45"
+
 API_KEY = ""
 
-url = "https://api.smartsentry.ai/v2/status?sentry_id="+sentry_id
+url = "https://api.smartsentry.ai/v2/status?sentry_id=" + sentry_id
 
 headers = {
  'x-api-key': API_KEY,
  'Content-Type': 'application/json'
 }
 
+// Call the status() function when starting the application in order to make sure settings are consistent with Sentry AI settings.
+// If user moves the application to a different machine, the status function allows the application to know the current settings.
 response = requests.request("GET", url, headers=headers)
 
-print(response.text)
+// code to get attributes for R6GNAS-B_Cam45
+
+
+// code to determine whether person identification is on for R6GNAS-B_Cam45
+if (person == "true")
+    print ("Person Identification is on")
+ else
+     print ("Person Identification is off")
+
+// code to determine whether face id is on for R6GNAS-B_Cam45
+if (face == "true")
+    print ("Person Identification is on")
+ else
+     print ("Person Identification is off")
 
 '''
 Sample Response:
