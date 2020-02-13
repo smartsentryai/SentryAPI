3b2535d397e5088ee9f98ce0b0ab0cd6a836843c
samuel-sentry <60991910+samuel-sentry@users.noreply.github.com>
2020-02-12 17:20:52 -0800

    Updates needed from Madhurya.

--- Status_Request.py
+++ Status_Request.py
@@ -1,18 +1,37 @@
 import requests
 
 sentry_id = "R6GNAS-B"
+camera = "Cam45"
 API_KEY = ""
 
-url = "https://api.smartsentry.ai/v2/status?sentry_id="+sentry_id
+url = "https://api.smartsentry.ai/v2/status?sentry_id=" + sentry_id
 
 headers = {
  'x-api-key': API_KEY,
  'Content-Type': 'application/json'
 }
 
+// The status() call synchronizes Sentry AI settings with the application.  Often called at application startup.
 response = requests.request("GET", url, headers=headers)
 
 print(response.text)
+
+// get attributes for camera
+<madhurya, write code to parse response to get Cam45 attributes>
+
+if (person == true)
+    print("Person detection is on")
+else
+    print("Person detection is off"
+          
+if (face == true)
+    print("Face ID is on")
+else
+    print("Face ID is off")
+
+          
+// The status call is also used to determine active services based on license status
+<madhurya right appropriate logic similar to above explaining what to do with trial and paid licenses>
 
 '''
 Sample Response:
